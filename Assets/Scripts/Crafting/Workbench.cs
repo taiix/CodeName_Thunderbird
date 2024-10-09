5ec77ff8e8af8d6d5a8e674e4ef73552d3a8d07b
Narniez <trifon221@gmail.com>
2024-10-09 18:27:51 +0300

    Workbench interaction demo done

--- Assets/Scripts/Crafting/Workbench.cs
+++ Assets/Scripts/Crafting/Workbench.cs
@@ -1,26 +1,47 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 public class Workbench : Interactable
 {
     public GameObject testingText;
 
     private Coroutine showTextCoroutine;
+
+    [SerializeField] CinemachineVirtualCamera workbenchCamera;
+
+    [SerializeField] Button exitButton;
+
+    bool isInteracting = false;
+
+    private void OnEnable()
+    {
+        exitButton.onClick.AddListener(DisableInteraction);
+    }
+
+    private void OnDisable()
+    {
+        exitButton.onClick.RemoveAllListeners();
+    }
+
     public override void OnFocus()
     {
-        interactionText = "Press 'F' to interact with Workbench.";
+        if (!isInteracting)
+        {
+
+            interactionText = "Press 'F' to interact with Workbench.";
+        }
     }
 
     public override void OnInteract()
     {
+        
+        InteractionHandler.Instance.HideInteractionUI();
+        isInteracting = true;
         Debug.Log("Interacting with Workbench");
-
-        if (showTextCoroutine != null)
-        {
-            StopCoroutine(showTextCoroutine);
-        }
-
-        showTextCoroutine = StartCoroutine(ShowTextTemporarily());
+        workbenchCamera.gameObject.SetActive(true);
     }
 
     public override void OnLoseFocus()
@@ -32,12 +53,19 @@ public class Workbench : Interactable
     void Start()
     {
         testingText.SetActive(false);
+        workbenchCamera.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (isInteracting)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            GameManager.Instance.DisablePlayerControls();
+            interactionText = string.Empty;
+        }
     }
 
     private IEnumerator ShowTextTemporarily()
@@ -49,6 +77,14 @@ public class Workbench : Interactable
         yield return new WaitForSeconds(1.5f);
 
         // Disable the text
-        testingText.SetActive(false);
+        interactionText = string.Empty;
+    }
+
+    void DisableInteraction()
+    {
+        isInteracting = false;
+        workbenchCamera.gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        GameManager.Instance.EnablePlayerControls();
     }
 }
