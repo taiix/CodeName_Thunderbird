29487b8cd397bb99f696fd6d4dd6463083e85e0b
Narniez <trifon221@gmail.com>
2024-11-07 23:28:14 +0200

    remove 3d textures

--- Assets/Scripts/Enemies/PursueState.cs
+++ Assets/Scripts/Enemies/PursueState.cs
@@ -19,7 +19,7 @@ public class PursueState : State
 
     public override void Enter()
     {
-        agent.speed = 20.5f;
+        agent.speed = 6.5f;
         Debug.Log("Agent speed set to: " + agent.speed);
         anim.SetTrigger("isRunning");
         agent.SetDestination(player.position);
