78301294fb47f52adb0ce95c13f12961f7a24c09
Stanislav <taix97@outlook.com>
2024-11-26 16:46:48 +0200

    New Quest

---
+++ Assets/Scripts/Quest/SO_Scripts/DestinationQuest_SO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Reaching Quest", menuName = "Quest/Reaching Quest")]
+public class DestinationQuest : BaseSO_Properties
+{
+    public Transform positionToReach;
+}
