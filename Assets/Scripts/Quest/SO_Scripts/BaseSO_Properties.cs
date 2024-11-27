f00d00fc8bfc2766d9eebb50533ec05e040d7db2
Stanislav <taix97@outlook.com>
2024-11-27 17:07:42 +0200

    New Quest system

--- Assets/Scripts/Quest/SO_Scripts/BaseSO_Properties.cs
+++ Assets/Scripts/Quest/SO_Scripts/BaseSO_Properties.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public abstract class BaseSO_Properties : ScriptableObject
 {
+    public string questName;
+    public string questDescription;
     public int id;
+
+    public bool isCompleted;
+
+    public abstract void Init();
+
+    public abstract void CheckProgress();
 }
